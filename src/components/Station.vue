ab38d88322a53015373c4f739d43dbad207b1b81
Matthias Cullmann <matthias.cullmann@gmail.com>
2024-12-29 23:00:08 +0100

    add song progress

--- src/components/Station.vue
+++ src/components/Station.vue
@@ -13,7 +13,11 @@
               :label="stationStore.stations[stationName].now.song.release.label"
               :albumTitle="stationStore.stations[stationName].now.song.release.title"
               :albumYear="stationStore.stations[stationName].now.song.year">
-              </StationItem>
+              <br />
+              <v-progress-linear :key="componentKey" :model-value="progress.percentage">
+              </v-progress-linear>
+                <div class="text-right" style="font-size: 12px;">{{ progress.elapsedTime }}/{{ progress.totalTime }}</div>
+            </StationItem>
           </v-list-item>
         </template>
       </v-list>
@@ -23,14 +27,64 @@
 
 
 <script setup>
-  import { useStationStore } from '@/stores/stationStore';
-  const stationStore = useStationStore();
+import { useStationStore } from '@/stores/stationStore';
+import { ref } from 'vue';
+const componentKey = ref(0);
+const stationStore = useStationStore();
 
-defineProps({
-    stationName: {
-        type: String,
-        required: true
-    }
+const props = defineProps({
+  stationName: {
+    type: String,
+    required: true
+  }
 });
+
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.round(time % 60);
+  return  `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
+const calculateProgress = () => {
+  const now = stationStore.stations[props.stationName].now
+  const nowTime = new Date().getTime();
+  const totalTime = now.endTime  - now.startTime ;
+  console.log("tt ", totalTime, props.stationName);
+  console.log("nt", (nowTime - now.startTime * 1000) / 1000, props.stationName);
+
+  const elapsedTime = (nowTime - now.startTime * 1000) / 1000
+  
+  return {
+    percentage: (elapsedTime / totalTime) * 100,
+    elapsedTime: formatTime(elapsedTime),
+    totalTime: formatTime(totalTime),
+  }
+};
+var progress = calculateProgress();
+
+import { onBeforeUnmount } from 'vue';
+
+const handleVisibilityChange = () => {
+  if (document.hidden) {
+    clearInterval(progressInterval);
+  } else {
+    progressInterval = setInterval(() => {
+      progress = calculateProgress();
+      componentKey.value += 1;
+    }, 1000);
+  }
+};
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
+
+onBeforeUnmount(() => {
+  document.removeEventListener('visibilitychange', handleVisibilityChange);
+  clearInterval(progressInterval);
+});
+
+let progressInterval = setInterval(() => {
+  progress = calculateProgress();
+  componentKey.value += 1;
+}, 1000);
 
 </script>
